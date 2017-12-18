3629ed0a34152465dd9ab32b8eabfd4e082c066e
hannushka <lindbladhannah@gmail.com>
2017-12-18 22:13:10 +0100

    Mock

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import ReactCardFlip from 'react-card-flip';
 import React, { Component } from 'react';
 import './App.css';
 
+import img1 from './stuga/STUGAN.JPG';
+import img2 from './stuga/BADTUNNA.JPG';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -20,11 +23,18 @@ class App extends React.Component {
     return (
       <ReactCardFlip isFlipped={this.state.isFlipped}>
         <div className="card" key="front" onClick={this.handleClick}>
-          <p>This is the front of the card.</p>
+        Häng med oss till spastuga!
+          {/*<div class="image"><img src={img1} alt="" /></div>
+          <div class="image"><img src={img2} alt="" /></div>*/}
         </div>
  
         <div className="card" key="back" onClick={this.handleClick}>
-          <p>This is the back of the card.</p>
+          <p>Datum: 19-21/1 2018</p>
+          <p>Var: Margretetorpsvägen 14, 297 95 Degeberga</p>
+          <p>SPA-WEEKEND MED BASTU & VEDELDAD BADTUNNA 
+          LAGA GOD MAT, VANDRA, BASTA/BADA & KOPPLA AV</p>
+          <p>Kramar från</p>
+          <p>Hannah & Ida</p>
         </div>
       </ReactCardFlip>
     )
